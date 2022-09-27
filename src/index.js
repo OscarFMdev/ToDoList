b6e543296102d0eec8b1b211d4f4c32e76bbcf80
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-09-27 15:53:16 -0500

    Add text input for tasks

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import './style.css';
 
+const tasksContainer = document.querySelector('.tasks-container');
+
 const tasks = [
   {
     index: 1,
     description: "Hello",
     completed: true
-  }
+  },
+  {
+    index: 2,
+    description: "World",
+    completed: true
+  },
+  {
+    index: 3,
+    description: "!!!!",
+    completed: true
+  },
 ]
+
+const createTasks = () => {
+  tasks.forEach((task) => {
+  });
+}
+
+createTasks();
