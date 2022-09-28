1e5273da3572f5f5ec15f8a2c3ae950006e75e39
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-09-28 15:08:57 -0500

    Render tasks

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const tasksContainer = document.querySelector('.tasks-container');
 
 const tasks = [
   {
+    index: 3,
+    description: '!!!!',
+    completed: true,
+  },
+  {
     index: 1,
     description: 'Hello',
     completed: true,
@@ -13,18 +18,14 @@ const tasks = [
     description: 'World',
     completed: true,
   },
-  {
-    index: 3,
-    description: '!!!!',
-    completed: true,
-  },
 ];
 
 const createTasks = () => {
+  tasks.sort((a, b) => a.index - b.index).map((task) => task.description);
   for (let i = 0; i < tasks.length; i += 1) {
     tasksContainer.innerHTML += `
     <!-- Task -->
-    <li class="task">
+    <li class="task index-${tasks[i].index}">
       <div class="left-side">
         <input type="checkbox">
         <p>${tasks[i].description}</p>
