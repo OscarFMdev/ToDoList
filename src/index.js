c40109b0bfe5417a199b0135c4ad6b4ea869d901
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-09-29 11:22:05 -0500

    Update file names

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './style.css';
-import displayTasks from './displayTasks';
+import displayTasks from './display-tasks';
 import {
   addTask, deleteTask, submit, tasksContainer,
-} from './addAndDelete';
+} from './add-and-delete';
 // import dragFeature from './dragAndDrop.js';
 
 /* Add and remove */
