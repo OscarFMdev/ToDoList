f02f0a347547d56b688749c198f8d326cb9d9ea3
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-10-01 18:51:41 -0500

    Add clear selected feature

--- src/index.js
+++ src/index.js
@@ -11,8 +11,18 @@ import { store, taskArray } from './modules/store.js';
 window.addEventListener('DOMContentLoaded', () => {
   const addBtn = document.querySelector('.fa-right-from-bracket');
   addBtn.addEventListener('click', (e) => {
+    window.location.reload();
     addTask(e);
   });
+
+  const trashCans = document.querySelectorAll('.delete-icon');
+  for (let i = 0; i < trashCans.length; i += 1) {
+    trashCans[i].addEventListener('click', (e) => {
+      window.location.reload();
+      displayTasks();
+      deleteTask(e);
+    });
+  }
 
   const taskTexts = document.querySelectorAll('p');
   for (let i = 0; i < taskTexts.length; i += 1) {
@@ -21,14 +31,21 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  const trashCans = document.querySelectorAll('.delete-icon');
-  for (let i = 0; i < trashCans.length; i += 1) {
-    trashCans[i].addEventListener('click', (e) => {
-      deleteTask(e);
-    });
-  }
-
+  const deleteCompleted = document.querySelector('.completed-text');
   const allCheckBoxes = document.querySelectorAll('input[type=checkbox]');
+
+  deleteCompleted.addEventListener('click', () => {
+    window.location.reload();
+    for (let i = 0; i < allCheckBoxes.length; i += 1) {
+      if (allCheckBoxes[i].checked === true) {
+        taskArray.splice(i, 1);
+        localStorage.setItem('taskInput', taskArray);
+        store();
+      }
+    }
+    displayTasks();
+  });
+
   for (let i = 0; i < allCheckBoxes.length; i += 1) {
     allCheckBoxes[i].addEventListener('click', (e) => {
       checkBoxesStatus(e);
@@ -36,17 +53,11 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-const deleteCompleted = document.querySelector('.completed-text');
-
-deleteCompleted.addEventListener('click', () => {
-  for (let i = 0; i < taskArray.length; i += 1) {
-    if (taskArray[i].completed === true) {
-      taskArray.splice(i - 1, 1);
-      localStorage.setItem('taskInput', taskArray);
-      store();
-      displayTasks();
-    }
-  }
+const deleteAll = document.querySelector('.reload-icon');
+deleteAll.addEventListener('click', () => {
+  taskArray.splice(0, taskArray.length);
+  store();
+  displayTasks();
 });
 
 displayTasks();
